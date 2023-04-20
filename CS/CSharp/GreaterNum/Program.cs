a289f38542d9b00dbec4b17d86f46c6ff460423a
Shaik <chandini.shaik3@dxc.com>
2023-04-20 13:34:42 +0530

    updated

---
+++ CS/CSharp/GreaterNum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace GreaterNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+            Console.WriteLine("Enter First Number");
+            n1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            n2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Third Number");
+            n3 = Convert.ToInt16(Console.ReadLine());
+            if(n1>n2 && n1 > n3)
+            {
+                Console.WriteLine("First Number is Greater");
+            }else if(n2>n1 && n2>n3)
+            {
+                Console.WriteLine("Second Number is Greater");
+            }else if(n3>n1 && n3>n2)
+            {
+                Console.WriteLine("Third Number is Greater");
+            }
+            else
+            {
+                Console.WriteLine("All are Equal");
+            }
+        }
+    }
+}
---
+++ CS/CSharp/Vowels/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Enter an Alphabet");
+            ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A'|| ch == 'E' || ch == 'I' || ch=='O' || ch == 'U')
+            {
+                Console.WriteLine("It is a Vowel");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Vowel");
+            }
+
+        }
+    }
+}
