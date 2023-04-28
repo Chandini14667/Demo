a44197d61440e93fbe9a46365e6820a16a1359ed
Shaik <chandini.shaik3@dxc.com>
2023-04-28 10:56:36 +0530

    updated

---
+++ CS/CSharp/GeneticsPractice/GeneticsPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GeneticsPractice
+{
+    class Demo<S>
+    {
+        S num;
+        public void SetValue(S value)
+        {
+            num = value;
+        }
+        public S GetValue()
+        {
+            return num;
+        }
+        
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo<int> d = new Demo<int>();
+            d.SetValue(20);
+            Console.WriteLine(d.GetValue());
+
+            Demo<string> d1 = new Demo<string>();
+            d1.SetValue("Chandini");
+            Console.WriteLine(d1.GetValue());
+
+            Demo<float> d2 = new Demo<float>();
+            d2.SetValue(2.4f);
+            Console.WriteLine(d2.GetValue());
+        }
+    }
+}
---
+++ CS/CSharp/InterfacePractice/InterfacePractice/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace InterfacePractice
+{
+    interface IFirstInterface
+    {
+        void sum(int a, int b);
+    }
+    //interface ISecondInterface:IFirstInterface 
+    interface ISecondInterface
+    {
+        void Sub(int a, int b);
+    }
+    class Demo: IFirstInterface,ISecondInterface 
+    {
+        public void sum(int a,int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
+            d.sum(1, 2);
+            d.Sub(2, 1);
+        }
+    }
+}
